612551324755fc4b9ada2c890e8c6354fe4872b0
Paul Crane <paul@acheronsoft.com>
2017-05-07 23:01:21 -0500

    Add support for setting callbacks in GetOptCodeGenerator.

--- source/ctoptions/getoptmixin.d
+++ source/ctoptions/getoptmixin.d
@@ -317,15 +317,78 @@ void generateGetOptCode(T, string varName = "options", string modName = __MODULE
 	}
 }
 
+private struct Callback(T)
+{
+	ReturnType!T opCall(Args...)(Args args)
+	{
+		static if(is(ReturnType!T == void))
+		{
+			if(callback_)
+			{
+				callback_(args);
+			}
+		}
+		else
+		{
+			ReturnType!T value;
+
+			if(callback_)
+			{
+				value = callback_(args);
+			}
+
+			return value;
+		}
+	}
+
+	Callback opAssign(T callback) pure @safe
+	{
+		set(callback);
+		return this;
+	}
+
+	void set(T callback) pure @safe
+	{
+		if(!isSet())
+		{
+			callback_ = callback;
+		}
+	}
+
+	T get()
+	{
+		return callback_;
+	}
+
+	bool isSet() pure const @safe
+	{
+		if(callback_)
+		{
+			return true;
+		}
+
+		return false;
+	}
+
+private:
+	T callback_;
+}
+
 class GetOptCodeGenerator(T, string varName = "options", string modName = __MODULE__)
 {
+	private alias VoidDelegate = void delegate();
+	private alias InvalidDelegate = void delegate(const string);
+	private alias HelpDelegate = void delegate(GetoptResult, CustomHelpFunction);
+
 	void generate(string[] arguments, ref T options, CustomHelpFunction func = &defaultGetoptPrinter)
 	{
+		initializeCallbacks();
+
 		try
 		{
 			if(arguments.length == 1)
 			{
-				onNoArguments();
+				onNoArguments_();
 			}
 			else
 			{
@@ -334,29 +397,29 @@ class GetOptCodeGenerator(T, string varName = "options", string modName = __MODU
 
 				if(helpInformation.helpWanted)
 				{
-					onHelp(helpInformation, func);
+					onHelp_(helpInformation, func);
 				}
 				else
 				{
-					onValidArgument();
+					onValidArgument_();
 				}
 			}
 		}
 		catch(GetOptException ex) // Called when unknown arg ie --flag is mispelled --flagg
 		{
-			onUnknownArgument(ex.msg);
+			onUnknownArgument_(ex.msg);
 		}
 		catch(ConvException ex) // Called when argument is passed a wrong type. int id; --id=hi
 		{
-			onInvalidArgument(ex.msg);
+			onInvalidArgument_(ex.msg);
 		}
 		catch(Exception ex)
 		{
-			onInvalidArgument(ex.msg);
+			onInvalidArgument_(ex.msg);
 		}
 	}
 
-	void onNoArguments() {}
+	void onNoArguments() { writeln("GetOptCodeGenerator.onNoArguments"); }
 
 	void onHelp(GetoptResult helpInformation, CustomHelpFunction func = &defaultGetoptPrinter)
 	{
@@ -375,4 +438,43 @@ class GetOptCodeGenerator(T, string varName = "options", string modName = __MODU
 		writeln("Invalid Argument!");
 		writeln(msg);
 	}
+
+	void setCallback(Func)(const string name, Func func)
+	{
+		final switch(name)
+		{
+			case "onNoArguments":
+				onNoArguments_ = func;
+				break;
+			case "onValidArgument":
+				onValidArgument_ = func;
+				break;
+			case "onUnknownArgument":
+				onUnknownArgument_ = func;
+				break;
+			case "onInvalidArgument":
+				onInvalidArgument_ = func;
+				break;
+			case "onHelp":
+				onHelp = func;
+				break;
+		}
+	}
+
+private:
+	void initializeCallbacks()
+	{
+		onNoArguments_ = &onNoArguments;
+		onValidArgument_ = &onValidArgument;
+		onUnknownArgument_ = &onUnknownArgument;
+		onInvalidArgument_ = &onInvalidArgument;
+		onHelp_ = &onHelp;
+	}
+
+private:
+	Callback!VoidDelegate onNoArguments_;
+	Callback!VoidDelegate onValidArgument_;
+	Callback!InvalidDelegate onUnknownArgument_;
+	Callback!InvalidDelegate onInvalidArgument_;
+	Callback!HelpDelegate onHelp_;
 }
